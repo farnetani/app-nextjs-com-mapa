f0e3b992ec3601717214c25f6c7599dc9eef464a
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-12 10:42:44 -0300

    Instalando graphql-request graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,28 @@
 import dynamic from 'next/dynamic'
+import LinkWrapper from 'components/LinkWrapper'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return (
-    <Map
-      places={[
-        {
-          id: '2',
-          name: 'reykjavik',
-          slug: 'reykjavik',
-          location: {
-            latitude: 45,
-            longitude: -10
-          }
-        }
-      ]}
-    />
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map
+      // places={[
+      //   {
+      //     id: '2',
+      //     name: 'reykjavik',
+      //     slug: 'reykjavik',
+      //     location: {
+      //       latitude: 45,
+      //       longitude: -10
+      //     }
+      //   }
+      // ]}
+      />
+    </>
   )
 }
---
+++ src/templates/About/index.tsx
@@ -0,0 +1,33 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} arial-label="Home" />
+    </LinkWrapper>
+    <S.Heading>My Trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Do elit ut excepteur duis cillum dolore nulla. Enim dolor ad anim non
+        voluptate. Sunt amet ut in commodo. Eu ex deserunt sit commodo nulla
+        nostrud cupidatat commodo qui. Velit est proident non laboris. Mollit
+        adipisicing voluptate ex minim enim dolor dolor veniam velit. Ex
+        reprehenderit eiusmod anim minim laboris excepteur non ea sunt. Labore
+        magna duis nostrud esse adipisicing. Ex excepteur officia voluptate
+        laborum minim proident ea dolore eu sit mollit non cillum. Excepteur
+        velit mollit laboris exercitation excepteur adipisicing ipsum consequat
+        culpa dolore. Cupidatat eiusmod exercitation ex laborum. Proident
+        commodo occaecat exercitation proident mollit dolore occaecat quis
+        cillum. Lorem sint ipsum fugiat sint cillum aute labore incididunt ea
+        excepteur. Aliquip proident esse dolore fugiat occaecat occaecat
+        proident ut sunt ex ea ad voluptate proident.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
