40c3569290366a340ca480ab650db32769a4d23f
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-16 11:12:46 -0300

    52. Utilizando MapConsumer do Leaflet para controlar mapa programaticamente

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/dist/client/router'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet'
 
 import * as S from './styles'
 
@@ -51,6 +51,22 @@ const Map = ({ places }: MapProps) => {
         ]}
         // worldCopyJump={true}
       >
+        <MapConsumer>
+          {(map) => {
+            // console.log(map)
+            // Capturo a largura da tela para verificar se deve diminuir o nível de zoom
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth
+
+            if (width < 768) {
+              map.setMinZoom(2)
+            }
+            return null
+          }}
+        </MapConsumer>
+
         <CustomTileLayer />
 
         {places?.map(({ id, slug, name, location }) => {
